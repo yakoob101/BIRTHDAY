1d216955de89fa45310d24a224caf47675a33fb9
Yakoob Ahmed <274354988+yakoob101@users.noreply.github.com>
2026-06-16 23:07:50 +0200

    Change text from 'I Have Something' to 'Click the Button!'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Click the Button!`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
